23edd2bc9ea530871283101030655e0216387f90
raushan1133 <raushankashu@gmail.com>
2024-08-31 02:19:49 +0530

    Fixed cookies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -46,8 +46,7 @@ const Home = () => {
   useEffect(()=>{
     const socketConnection = io(process.env.REACT_APP_BACKEND_URL,{
       auth : {
-        token : localStorage.getItem('token'),
-        withCredentials:true
+        token : localStorage.getItem('token')
       },
     })
 
